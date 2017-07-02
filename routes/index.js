a542530becb7c71932b4f9f67a1c73258f8ed8ed
Neil Palima <neiljan.palima@gmail.com>
2017-07-02 12:05:07 +0800

    Renamed jade to pug files. Added css and pug file for new pages

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,15 @@ var express =  require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('cover');
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.get('/products', (req, res) => {
+    res.render('products');
 });
 
 router.get('/partial/:name', (req, res) => {
